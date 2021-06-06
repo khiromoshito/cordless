947921e4b1bc1ca11718bedb07554beb32facb38
khiromoshito <labanan02@gmail.com>
2021-06-06 09:27:54 +0800

    added EventHandler

---
+++ bin/handlers/EventHandler.js
@@ -0,0 +1,18 @@
+const EventHandler = {
+    counter: 0,
+    clients: {},
+    
+    push: (clientId, event, listener) => {
+        const clientEvents = EventHandler.clients[clientId];
+        if(clientEvents[event]===undefined) clientEvents[event] = [];
+        clientEvents[event].push(listener); 
+    },
+
+    emit: (clientId, event, args) => {
+        for(const listener of (EventHandler.clients[clientId]?.[event] || [])) {
+            listener(...args);
+        }
+    }
+};
+
+module.exports = EventHandler;
